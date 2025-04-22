b15f85763624197227e88b121b38b3f29e158eb2
Satyam Mishra <satyam1308mishra@gmail.com>
2025-04-22 14:49:53 +0530

    feat: lay out initial graphql setup

--- src/graphql/eas/queries/ecocerts-by-project.ts
+++ src/graphql/eas/queries/ecocerts-by-project.ts
@@ -1,16 +1,14 @@
-import { graphql, ResultOf } from "gql.tada";
+// import { graphql, ResultOf } from "gql.tada";
 
-const EcocertsByProjectQuery = graphql(`
-  query GetEcocertsInProject($id: BigInt!) {
-    ecocertsInProject(id: $id) {
-      attestation_id
-      attester
-      ecocert_id
-      id
-      project_id
-      timestamp
-    }
-  }
-`);
-
-type EcocertsByProjectQueryResponse = ResultOf<typeof EcocertsByProjectQuery>;
+// const EcocertsByProjectQuery = graphql(`
+//   query GetEcocertsInProject($id: BigInt!) {
+//     ecocertsInProject(id: $id) {
+//       attestation_id
+//       attester
+//       ecocert_id
+//       id
+//       project_id
+//       timestamp
+//     }
+//   }
+// `);
